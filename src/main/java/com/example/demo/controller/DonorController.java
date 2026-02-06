7c8574fd0d3052a0173566b3526b35f0ea870cef
vasanth005-117 <23suca26@tcarts.in>
2026-02-06 09:44:47 +0530

    Updated DonorController

--- src/main/java/com/example/demo/controller/DonorController.java
+++ src/main/java/com/example/demo/controller/DonorController.java
@@ -22,7 +22,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/donors")
+@RequestMapping("/api/donors-new")
 @RequiredArgsConstructor
 public class DonorController {
     
